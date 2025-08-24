908ecbca6b6f9f4f7692fe337704688b3a351458
Meera Santram Chavan <chavanmeera66@gmail.com>
2025-08-24 12:11:29 +0530

    Update MeeraInfoJDBC.java

--- MeeraInfoJDBC.java
+++ MeeraInfoJDBC.java
@@ -1,7 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.Statement; 
 
 public class MeeraInfoJDBC {
     public static void main(String[] args) {
